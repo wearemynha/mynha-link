c353bb87373c35710aa48af2c309768e7eebb656
Leonardo Vieira <leonardo@mynha.com.br>
2026-08-26 17:43:33 -0300

    migrate frontend tooling to Vite and update dependencies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,9 +15,7 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-$app->bind('path.public', function () {
-    return dirname(__DIR__);
-});
+$app->usePublicPath(dirname(__DIR__));
 
 /*
 |--------------------------------------------------------------------------
