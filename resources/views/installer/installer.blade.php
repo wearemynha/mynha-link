9df47f3a1f7bc06cc080d18a57d2137e240906a2
Leonardo Vieira <leonardo@mynha.com.br>
2026-09-03 23:46:58 -0300

    feat: restyle Mynha Link installer

--- resources/views/installer/installer.blade.php
+++ resources/views/installer/installer.blade.php
@@ -17,28 +17,27 @@ $queryString = request()->server('QUERY_STRING', '');
     @endphp
 @else
         <div class="logo-container fadein">
-           <img class="logo-img" src="{{ asset('assets/linkstack/images/logo.svg') }}" alt="Logo">
+           <img class="logo-img" src="{{ asset('assets/mynha-assets/mynha-logo-installer.svg') }}" alt="Mynha">
         </div>
         <h1>{{__('messages.Setup LinkStack')}}</h1>
-        <p class="inst-txt">
-        <div class="left-txt glass-container">
+        <div class="left-txt glass-container mynha-panel installer-welcome">
          {{__('messages.Welcome to the setup for LinkStack!')}}<br><br>
         <b>{{__('messages.This setup will:')}}</b><br>
         {{__('messages.Check the server dependencies')}}<br>
         {{__('messages.Setup the database')}}<br>
         {{__('messages.Create the admin user')}}<br>
         {{__('messages.Configure the app')}}<br>
-        </div></p>  
+        </div>
         
 {{-- start language --}}
 <?php $configValue2 = config('app.locale'); ?>
 <form id="language-form" action="{{route('editConfigInstaller')}}" enctype="multipart/form-data" method="post">
     <div class="form-group col-lg-8">
-        <input value="homeurl" name="type" style="display:none;" type="text" class="form-control form-control-lg" required>
-        <input value="LOCALE" name="entry" style="display:none;" type="text" class="form-control form-control-lg" required>
-        <p class="text-muted">{{__('messages.Choose a language')}}</p>
+        <input value="homeurl" name="type" type="hidden">
+        <input value="LOCALE" name="entry" type="hidden">
+        <label for="installer-language" class="text-muted">{{__('messages.Choose a language')}}</label>
         <div class="input-group">
-            <select style="max-width:600px;min-width:300px;" class="form-control" name="value">
+            <x-mynha.select id="installer-language" class="mynha-field-surface" name="value">
                 @if($configValue2 != '')
                     <option>{{$configValue2}}</option>
                 @endif
@@ -56,7 +55,7 @@ $queryString = request()->server('QUERY_STRING', '');
                     }
                 }
                 ?>
-            </select>
+            </x-mynha.select>
         </div>
     </div>
     <input type="hidden" name="_token" value="{{csrf_token()}}">
@@ -68,9 +67,9 @@ $queryString = request()->server('QUERY_STRING', '');
 </form>
 {{-- end language --}}
 
-<p style="margin:25px;max-width:350px;">{{__('messages.setup.disclaimer')}} <a href="https://linkstack.org/terms-and-conditions/" target="_blank">{{__('messages.Terms and Conditions')}}</a>.</p>
+<p class="installer-disclaimer">{{__('messages.setup.disclaimer')}} <a href="https://linkstack.org/terms-and-conditions/" target="_blank">{{__('messages.Terms and Conditions')}}</a>.</p>
 
-        &ensp;<a class="btn" href="{{url('?2')}}"><button>{{__('messages.Next')}}</button></a>&ensp;
+        <x-mynha.button :href="url('?2')">{{__('messages.Next')}}</x-mynha.button>
 @endif
       
 @endif
@@ -79,12 +78,12 @@ $queryString = request()->server('QUERY_STRING', '');
 {{-- Landing page --}}
         
         <div class="logo-container fadein">
-           <img class="logo-img" src="{{ asset('assets/linkstack/images/logo.svg') }}" alt="Logo">
+           <img class="logo-img" src="{{ asset('assets/mynha-assets/mynha-logo-installer.svg') }}" alt="Mynha">
         </div>
         <h1>{{__('messages.Setup failed')}}</h1>
         <p class="inst-txt">{{__('messages.An error has occured. Please try again')}}</p>
         <div class="row">
-        &ensp;<a class="btn" href="{{url('?3')}}"><button>{{__('messages.Try again')}}</button></a>&ensp;
+        <x-mynha.button :href="url('?3')">{{__('messages.Try again')}}</x-mynha.button>
         </div>
       
 @endif
@@ -93,13 +92,12 @@ $queryString = request()->server('QUERY_STRING', '');
 {{-- Landing page --}}
         
         <div class="logo-container fadein">
-           <img class="logo-img" src="{{ asset('assets/linkstack/images/logo.svg') }}" alt="Logo">
+           <img class="logo-img" src="{{ asset('assets/mynha-assets/mynha-logo-installer.svg') }}" alt="Mynha">
         </div>
         <h1>{{__('messages.Dependency check')}}</h1>
         <p class="inst-txt">{{__('messages.Required PHP modules:')}}</p>
-        <div class="left-txt glass-container">
-        <table style="width:115%">
-        <style>.bi-x-lg{color:tomato}</style>
+        <div class="left-txt glass-container mynha-panel">
+        <table>
         <tr><td>BCMath: </td><td>@if(extension_loaded('bcmath'))<i class="bi bi-check-lg"></i>@else<i class="bi bi-x-lg"></i>@endif</td></tr>
         <tr><td>Ctype: </td><td>@if(extension_loaded('Ctype'))<i class="bi bi-check-lg"></i>@else<i class="bi bi-x-lg"></i>@endif</td></tr>
         <tr><td>cURL: </td><td>@if(extension_loaded('cURL'))<i class="bi bi-check-lg"></i>@else<i class="bi bi-x-lg"></i>@endif</td></tr>
@@ -115,13 +113,13 @@ $queryString = request()->server('QUERY_STRING', '');
         <tr><td>XML: </td><td>@if(extension_loaded('XML'))<i class="bi bi-check-lg"></i>@else<i class="bi bi-x-lg"></i>@endif</td></tr>
         </table>
         <br>
-        <b style="font-size:90%;margin-bottom:5px;display:flex;">{{__('messages.Depending on your database type:')}}</b>
-        <table style="width:123%">
+        <b class="installer-dependency-note">{{__('messages.Depending on your database type:')}}</b>
+        <table>
         <tr><td>PostgreSQL: </td><td>@if(extension_loaded('pdo_pgsql'))<i class="bi bi-check-lg"></i>@else<i class="bi bi-x-lg"></i>@endif</td></tr>
         </table>
         </div><br>
         <div class="row">
-        &ensp;<a class="btn" href="?3"><button>{{__('messages.Next')}}</button></a>&ensp;
+        <x-mynha.button href="?3">{{__('messages.Next')}}</x-mynha.button>
         </div>
       
 @endif
@@ -130,14 +128,14 @@ $queryString = request()->server('QUERY_STRING', '');
 {{-- Database preparation --}}
 
         <div class="logo-container fadein">
-           <img class="logo-img" src="{{ asset('assets/linkstack/images/logo.svg') }}" alt="Logo">
+           <img class="logo-img" src="{{ asset('assets/mynha-assets/mynha-logo-installer.svg') }}" alt="Mynha">
         </div>
         <h1>{{__('messages.Setup LinkStack')}}</h1>
         <p class="inst-txt">{{__('messages.Setup the database')}}</p>
         <p>{{__('messages.PostgreSQL configuration is provided through environment variables.')}}</p>
         <form id="prepare-database-form" action="{{route('prepareDatabase')}}" method="post">
             <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <button type="submit" class="mt-3 ml-3 btn btn-info">{{__('messages.Next')}}</button>
+            <x-mynha.button type="submit">{{__('messages.Next')}}</x-mynha.button>
         </form>
 
 @endif
@@ -146,30 +144,34 @@ $queryString = request()->server('QUERY_STRING', '');
 {{-- Landing page --}}
         
         <div class="logo-container fadein">
-           <img class="logo-img" src="{{ asset('assets/linkstack/images/logo.svg') }}" alt="Logo">
+           <img class="logo-img" src="{{ asset('assets/mynha-assets/mynha-logo-installer.svg') }}" alt="Mynha">
         </div>
         <h1>{{__('messages.Setup LinkStack')}}</h1>
         <p class="inst-txt">{{__('messages.Create an admin account')}}</p>
 
 <form id="home-url-form" action="{{route('createAdmin')}}" enctype="multipart/form-data" method="post">
 <div class="form-group col-lg-8">
-<label>{{__('messages.Admin email:')}}</label>
-<input style="max-width:275px;" class="form-control" placeholder="admin@example.com" name="email" type="email" autocomplete="email" required>
-<label>{{__('messages.Admin password:')}}</label>
-<input style="max-width:275px;" class="form-control" name="password" type="password" minlength="12" autocomplete="new-password" required>
-<label>{{__('messages.Confirm Password')}}:</label>
-<input style="max-width:275px;" class="form-control" name="password_confirmation" type="password" minlength="12" autocomplete="new-password" required>
-<label>{{__('messages.Handle:')}}</label>
-<div class="input-group">
-<div class="input-group-prepend"><div class="input-group-text">@</div></div>
-<input style="max-width:237px; padding-left:50px;" class="form-control" name="handle" type="text" required>
+<x-mynha.field for="admin-email" :label="__('messages.Admin email:')">
+    <x-mynha.input id="admin-email" placeholder="admin@example.com" name="email" type="email" autocomplete="email" required />
+</x-mynha.field>
+<x-mynha.field for="admin-password" :label="__('messages.Admin password:')">
+    <x-password-field id="admin-password" name="password" minlength="12" autocomplete="new-password" required />
+</x-mynha.field>
+<x-mynha.field for="admin-password-confirmation" :label="__('messages.Confirm Password').':'">
+    <x-password-field id="admin-password-confirmation" name="password_confirmation" minlength="12" autocomplete="new-password" required />
+</x-mynha.field>
+<x-mynha.field for="admin-handle" :label="__('messages.Handle:')">
+    <div class="mynha-input-group">
+        <span class="mynha-input-group__prefix" aria-hidden="true">@</span>
+        <x-mynha.input id="admin-handle" name="handle" type="text" required />
+    </div>
+</x-mynha.field>
+<x-mynha.field for="admin-name" :label="__('messages.Name:')">
+    <x-mynha.input id="admin-name" name="name" type="text" required />
+</x-mynha.field>
 </div>
-<label>{{__('messages.Name:')}}</label>
-<input style="max-width:275px;" class="form-control" name="name" type="text" required>
-<div class="input-group">
-</div></div><br>
 <input type="hidden" name="_token" value="{{csrf_token()}}">
-<button type="submit" class="mt-3 ml-3 btn btn-info">{{__('messages.Next')}}</button>
+<x-mynha.button type="submit">{{__('messages.Next')}}</x-mynha.button>
 </form>
       
 @endif
@@ -178,31 +180,32 @@ $queryString = request()->server('QUERY_STRING', '');
 {{-- Landing page --}}
         
         <div class="logo-container fadein">
-           <img class="logo-img" src="{{ asset('assets/linkstack/images/logo.svg') }}" alt="Logo">
+           <img class="logo-img" src="{{ asset('assets/mynha-assets/mynha-logo-installer.svg') }}" alt="Mynha">
         </div>
         <h1>{{__('messages.Setup LinkStack')}}</h1>
         <p class="inst-txt">{{__('messages.Configure your page')}}</p>
 <form id="home-url-form" action="{{route('options')}}" enctype="multipart/form-data" method="post">
 <div class="form-group col-lg-8">
-<div class="input-group">
+<x-mynha.field for="registration-option" :label="__('messages.Enable registration:')">
+    <x-mynha.select id="registration-option" name="register">
+        <option value="Yes">{{__('messages.Yes')}}</option>
+        <option value="No">{{__('messages.No')}}</option>
+    </x-mynha.select>
+</x-mynha.field>
 
-<label>{{__('messages.Enable registration:')}}</label>
-<select style="max-width:300px" class="form-control" name="register">
-<option value="Yes">{{__('messages.Yes')}}</option>
-<option value="No">{{__('messages.No')}}</option>
-</select>
+<x-mynha.field for="verification-option" :label="__('messages.Enable email verification:')">
+    <x-mynha.select id="verification-option" name="verify">
+        <option value="Yes">{{__('messages.Yes')}}</option>
+        <option value="No">{{__('messages.No')}}</option>
+    </x-mynha.select>
+</x-mynha.field>
 
-<label>{{__('messages.Enable email verification:')}}</label>
-<select style="max-width:300px" class="form-control" name="verify">
-<option value="Yes">{{__('messages.Yes')}}</option>
-<option value="No">{{__('messages.No')}}</option>
-</select>
-
-<label>{{__('messages.Set your page as Home Page')}}</label>
-<select id="select" style="max-width:300px" class="form-control" name="page">
-<option value="No">{{__('messages.No')}}</option>
-<option value="Yes">{{__('messages.Yes')}}</option>
-</select>
+<x-mynha.field for="select" :label="__('messages.Set your page as Home Page')">
+    <x-mynha.select id="select" name="page">
+        <option value="No">{{__('messages.No')}}</option>
+        <option value="Yes">{{__('messages.Yes')}}</option>
+    </x-mynha.select>
+</x-mynha.field>
 <script src="{{ asset('assets/external-dependencies/jquery-3.4.1.min.js') }}"></script>
 <script src="{{ asset('assets/external-dependencies/sweetalert2.min.js') }}"></script>
 <script>
@@ -222,12 +225,12 @@ $("#select").change(function(){
 });
 </script>
 
-<label>{{__('messages.App Name:')}}</label>
-<input style="max-width:275px;" class="form-control" value="LinkStack" name="app" type="text" required>
-
-</div></div><br>
+<x-mynha.field for="app-name" :label="__('messages.App Name:')">
+    <x-mynha.input id="app-name" value="Mynha Link" name="app" type="text" required />
+</x-mynha.field>
+</div>
 <input type="hidden" name="_token" value="{{csrf_token()}}">
-<button type="submit" class="mt-3 ml-3 btn btn-info">{{__('messages.Finish setup')}}</button>
+<x-mynha.button type="submit">{{__('messages.Finish setup')}}</x-mynha.button>
 </form>
       
 @endif
